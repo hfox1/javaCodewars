5f94b44db651149c5126ab1715b4d4bbd6f86922
Harry Fox <49591810+hfox1@users.noreply.github.com>
2023-02-28 16:42:16 +0000

    more codewars complete

--- Katas/app/src/test/java/Katas/KatasTest.java
+++ Katas/app/src/test/java/Katas/KatasTest.java
@@ -40,6 +40,21 @@ public class KatasTest {
       assertEquals("Nope!", 168, kat.getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"));
     
 }
+    @Test public void testDisemvowel() {
+        Katas kat = new Katas();
+        assertEquals("Ths wbst s fr lsrs LL!",kat.disemvowel("This website is for losers LOL!")
+                                      );
+        assertEquals("N ffns bt,\nYr wrtng s mng th wrst 'v vr rd", kat.disemvowel(
+              "No offense but,\nYour writing is among the worst I've ever read"));
+        assertEquals( "Wht r y,  cmmnst?", kat.disemvowel("What are you, a communist?"));
+    
+}
+    @Test public void testDigitSquared() {
+        Katas kat = new Katas();
+        assertEquals(811181, kat.squareDigits(9119));
+        assertEquals(0, kat.squareDigits(0));
+  
+}
     @Test public void test() {
         Katas kat = new Katas();
 }
